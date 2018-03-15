87b1c98e37223b6657afbf968fb64640afdeffe7
Viru <OTAKUPENGUINOP@GMAIL.COM>
2018-03-15 13:58:04 -0400

    Update heap.h -- Needs List Constructor ^ Refactor

--- Structures/Heap/Heap.h
+++ Structures/Heap/Heap.h
@@ -7,7 +7,7 @@ class Heap{
 
 	private:
 		std::vector<T> heap;
-		bool minType; // If false, max type.
+		bool minType; // If false, max is at root.
 		int capacity;
 		int currentSize;
 		 
@@ -17,16 +17,16 @@ class Heap{
 			this->heap[pos2] = temp;
 		}
 		
-		void validateInterface(int where) {
+		void heapifyInsert(int where) {
 			if (this->minType) {
-				validateMin(where);
+				heapifyMinIns(where);
 			}
 			else {
-				validateMax(where);
+				heapifyMaxIns(where);
 			}
 		}
 
-		void validateMin(int where) {
+		void heapifyMinIns(int where) {
 
 			int parentLoc = parent(where);
 			T parent = this->heap[parentLoc];
@@ -37,11 +37,11 @@ class Heap{
 			}
 			else {
 				swap(parentLoc, where);
-				validateMin(parentLoc);
+				heapifyMinIns(parentLoc);
 			}
 		}
 
-		void validateMax(int where) {
+		void heapifyMaxIns(int where) {
 
 			int parentLoc = parent(where);
 			T parent = this->heap[parentLoc];
@@ -52,8 +52,111 @@ class Heap{
 			}
 			else {
 				swap(parentLoc, where);
-				validateMax(parentLoc);
+				heapifyMaxIns(parentLoc);
 			}
+		}
+
+		void heapifyMinDel(int where) {
+
+			int rightChildLoc = (where + 1) * 2;
+			int leftChildLoc = rightChildLoc - 1;
+
+
+			if (leftChildLoc > this->currentSize - 1) {
+				// The node at WHERE has no children. Property is good.
+				return;
+			}
+			else if (rightChildLoc > this->currentSize - 1) {
+				// The node has 1 child.
+
+				if (this->heap[leftChildLoc] < this->heap[where]) {
+					// If the child of the node is less than the parent!
+					swap(leftChildLoc, where); // Swap the child and parent.
+					heapifyMinDel(leftChildLoc);
+				}
+				else {
+					return; // The property is satisfied.
+				}
+			}
+			else {
+				// The node has 2 children.
+				// Find the lower child: 
+				if (this->heap[leftChildLoc] > this->heap[rightChildLoc]) {
+					// the right child is the smaller child.
+					if (this->heap[rightChildLoc] < this->heap[where]) {
+						// The right child is smaller than the parent.
+						swap(rightChildLoc, where);
+						heapifyMinDel(rightChildLoc);
+					}
+					else {
+						return; // The property is valid.
+					}
+				}
+				else {
+					// the left child is the smaller child.
+					if (this->heap[leftChildLoc] < this->heap[where]) {
+						// The left child could be the new minima.
+						swap(leftChildLoc, where);
+						heapifyMinDel(leftChildLoc);
+					}
+					else {
+						return; // The property is valid.
+					}
+				}
+			}
+		}
+
+		void heapifyMaxDel(int where) {
+
+			int rightChildLoc = (where + 1) * 2;
+			int leftChildLoc = rightChildLoc - 1;
+
+
+			if (leftChildLoc > this->currentSize - 1) {
+				// The node at WHERE has no children. Property is good.
+				return;
+			}
+			else if (rightChildLoc > this->currentSize - 1) {
+				// The node has 1 child.
+
+				if (this->heap[leftChildLoc] > this->heap[where]) {
+					// If the child of the node is less than the parent!
+					swap(leftChildLoc, where); // Swap the child and parent.
+					heapifyMaxDel(leftChildLoc);
+				}
+				else {
+					return; // The property is satisfied.
+				}
+			}
+			else {
+				// The node has 2 children.
+				// Find the lower child: 
+				if (this->heap[leftChildLoc] < this->heap[rightChildLoc]) {
+					// the right child is the smaller child.
+					if (this->heap[rightChildLoc] > this->heap[where]) {
+						// The right child is smaller than the parent.
+						swap(rightChildLoc, where);
+						heapifyMaxDel(rightChildLoc);
+					}
+					else {
+						return; // The property is valid.
+					}
+				}
+				else {
+					// the left child is the smaller child.
+					if (this->heap[leftChildLoc] > this->heap[where]) {
+						// The left child could be the new minima.
+						swap(leftChildLoc, where);
+						heapifyMaxDel(leftChildLoc);
+					}
+					else {
+						return; // The property is valid.
+					}
+				}
+			}
+
+
+
 		}
 
 		int parent(int where) {
@@ -86,15 +189,47 @@ class Heap{
 
 		void insert(T what) {
 			if (this->currentSize >= this->capacity) {
-				std::cout << "\n-------\nERR: The heap is full, or insertion too big.\n-------\n";
+				std::cout << "\n<ERR: The heap is full, or insertion too big.>\n";
 				return;
 			}
 			else {
 				this->heap.push_back(what);
-				validateInterface(currentSize);
+				heapifyInsert(currentSize);
 				++(this->currentSize);
 			}
 		}
 
+		void deleteRoot() {
 
+			if (this->currentSize == 0) {
+				// Tree is empty.
+				return;
+			}
+
+			else {
+
+				this->heap[0] = this->heap[this->currentSize -1];
+				this->heap.pop_back();
+				--(this->currentSize);
+
+
+				if (this->minType == true) {
+					heapifyMinDel(0);
+				}
+
+				else {
+					heapifyMaxDel(0);
+				}
+			}
+		}
+		
+		T getRoot() {
+			if (this->currentSize == 0) {
+				// Tree is empty
+				throw;
+			}
+			else {
+				return this->heap[0];
+			}
+		}
 };

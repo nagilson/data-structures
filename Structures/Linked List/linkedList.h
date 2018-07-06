b02090baf821e40b33c3c359050d9f78d836cd06
Noah <OTAKUPENGUINOP@GMAIL.COM>
2018-07-06 16:17:57 -0400

    removed vs references

--- Structures/Linked List/linkedList.h
+++ Structures/Linked List/linkedList.h
@@ -7,7 +7,6 @@ Node<T> values with pointers and a data type.
 */
 /// ------------------------------------------------------------------------------------ ///
 
-// #include "stdafx.h" // You may want to include this if you create a VS PRJ with cmake.
 #include <iostream>
 #include <exception>
 

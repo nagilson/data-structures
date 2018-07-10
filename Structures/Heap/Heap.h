4b9d1b55e1cd492ec0f30f1e219c6fe822eda0db
Noah <OTAKUPENGUINOP@GMAIL.COM>
2018-07-10 09:44:54 -0400

    Update Heap.h

--- Structures/Heap/Heap.h
+++ Structures/Heap/Heap.h
@@ -300,7 +300,7 @@ class Heap{
 
 			/// ------------------------------------------------------------------------------------ ///
 			/*
-			Print the heap in an array / list liek representation.
+			Print the heap in an array / list like representation.
 			*/
 			/// ------------------------------------------------------------------------------------ ///
 
